ac113aa35d36a5126ad9e6dad18806a6d1d66f67
Chloebalmes <chloe.balmes@ensta-paris.fr>
2021-05-11 17:54:38 +0200

    Deplacement avec la souris

--- projet/simulator/graphics/screen.py
+++ projet/simulator/graphics/screen.py
@@ -19,8 +19,8 @@ class Screen:
         self.mouse_position = Vector2(0, 0)
 
         # this will store which buttons have been pressed in the current frame
-        # [LeftMouse, MiddleMouse, RightMouse, ScrollWheelUp, ScrollWheelDown]
-        self._buttons = [False, False, False, False, False]
+        # [LeftMouse, MiddleMouse, RightMouse, ScrollWheelUp, ScrollWheelDown,Left_Key,Right_Key,Up_Key,Down_Key]
+        self._buttons = [False, False, False, False, False,False,False,False,False]
 
         # this will be true when the user presses the exit button of the window
         self.should_quit = False
@@ -47,10 +47,22 @@ class Screen:
         for event in events:
             if event.type == pg.QUIT:
                 self.should_quit = True
-            elif event.type == pg.MOUSEBUTTONDOWN:
+            elif event.type == pg.MOUSEBUTTONDOWN: #On clique avec la souris
                 if event.button <= len(self._buttons):
                     self._buttons[event.button - 1] = True
-
+            elif event.type == pg.KEYDOWN: #On se déplace avec ZQSD
+                if event.key == pg.K_z:
+                    self._buttons[7]=True
+                    self.camera.position[1]+=0.1
+                if event.key == pg.K_q:
+                    self._buttons[5]=True
+                    self.camera.position[0]+=0.1
+                if event.key == pg.K_s:
+                    self._buttons[8]=True
+                    self.camera.position[1]-=0.1
+                if event.key == pg.K_d:
+                    self._buttons[6]=True
+                    self.camera.position[0]-=0.1
         self.frame += 1
 
     def draw(self, world):
@@ -81,10 +93,11 @@ class Screen:
 
     def __draw_world(self, s, world):
         for body in world.bodies():
-            screen_pos = self.camera.to_screen_coords(body.position)
-            pg.draw.circle(s, body.color,
-                           (int(screen_pos.get_x()), int(screen_pos.get_y())),
+                screen_pos = self.camera.to_screen_coords(body.position)
+                pg.draw.circle(s, body.color,
+                        (int(screen_pos.get_x()), int(screen_pos.get_y())),
                            int(body.draw_radius), 0)
+
 
     def draw_corner_text(self, s):
         draw_text(self._screen, self._font, s,
@@ -95,3 +108,7 @@ class Screen:
     def get_right_mouse(self): return self._buttons[2]
     def get_wheel_up(self): return self._buttons[3]
     def get_wheel_down(self): return self._buttons[4]
+    def get_left_key(self):return self.buttons[5]
+    def get_right_key(self): return self.buttons[6]
+    def get_up_key(self) : return self.buttons[7]
+    def get_down_key(self):return self.buttons[8]
